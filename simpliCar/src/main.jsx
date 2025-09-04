f97f441315cf89492da47d80c3f7acc6d55e4e74
Caio Dicatti <caio_dicatti@hotmail.com>
2025-09-04 20:07:59 -0300

    feat: add hashrouter

--- simpliCar/src/main.jsx
+++ simpliCar/src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom'; - removido o HashRouter
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 
-
+/* mudando para amazon, voltar para BrowserRouter
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/simplicar-frontend">
     <App />
   </BrowserRouter>
+)
+  */
+
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
